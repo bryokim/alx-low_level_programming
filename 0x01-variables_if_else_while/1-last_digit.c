e010f00c098a3c2508b661e4f82d65d0b649f4fd
Brian Kimathi <bryo.kim1@gmail.com>
2022-12-10 06:19:27 -0800

    Changed 1-last-digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,9 +27,9 @@ int main(void)
 	if (lastDigit > 5)
 		printf("and is greater than 5\n");
 	else if (lastDigit == 0)
-		printf("and is zero\n");
+		printf("and is 0\n");
 	else
-		printf("and is less than 6 and not zero\n");
+		printf("and is less than 6 and not 0\n");
 
 	return (0);
 }

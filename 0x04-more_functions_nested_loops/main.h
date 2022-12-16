e5e32413aeb197c80da33a471d14af99843b8006
Brian Kimathi <bryo.kim1@gmail.com>
2022-12-15 17:26:34 -0800

    Corrected main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@ int _putchar(int);
 int _isupper(int);
 int mul(int, int);
 int _isdigit(int);
-int print_numbers(void);
+void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int);
 void print_diagonal(int);

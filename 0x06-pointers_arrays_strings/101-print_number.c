11c41592d2ee0ac29935840e35b7b2e9ab17fdfa
Brian Kimathi <bryo.kim1@gmail.com>
2022-12-22 16:34:07 -0800

    Added 101-print_number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ *
+ * Description: prints an integer
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 10)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}

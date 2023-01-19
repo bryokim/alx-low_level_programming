5b4503ef7c189ece8cdd88305384209353c0711b
Brian Kim <bryo.kim1@gmail.com>
2023-01-19 18:14:33 +0300

    Add 100-main_opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < atoi(argv[1]); i++)
+	{
+		printf("%02x", *((unsigned char *)main + i));
+
+		if (i < (atoi(argv[1]) - 1))
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

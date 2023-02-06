bdd4661f7e8ceb79314b5704aa1820ab07548112
Brian Kim <bryo.kim1@gmail.com>
2023-02-07 00:06:57 +0300

    Added 100-get_endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+
+/**
+ * get_endianness - get the endianness
+ *
+ * Return: 1 if little endian, 0 if big endian.
+*/
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *p = (char *)&n;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
+}

1df2fa7f3173a1bc68f7419fd9ab228ca49ec78e
Brian Kim <bryo.kim@gmail.com>
2023-03-16 20:59:59 +0300

    Added ability to insert at end of list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,7 +3,32 @@
 #include <stdio.h>
 
 /**
- * insert_dnodeint - insert a node at index given.Index must be greater than 0.
+ * insert_middle - Insert a node in the middle of the list.
+ *
+ * @temp: Pointer to the node before which to add the new node.
+ * @n: Data for the new node.
+ * Return: Address of the new node or NULL if an error occurred.
+ */
+dlistint_t *insert_middle(dlistint_t *temp, int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->next = temp;
+	new_node->prev = temp->prev;
+	temp->prev->next = new_node;
+	temp->prev = new_node;
+	new_node->n = n;
+
+	return (new_node);
+}
+
+/**
+ * insert_index_not_zero - insert a node at index given.
+ * Index must be greater than 0.
  *
  * @head: Pointer to the pointer of the start of the list.
  * @idx: Index to add the new node. Must be greater than 0.
@@ -11,35 +36,25 @@
  * Return: Address of the new node or NULL if it failed or if node
  * cannot be added at given index.
  */
-dlistint_t *insert_dnodeint(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_index_not_zero(dlistint_t **head, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp;
+	dlistint_t *temp, *new_node;
 	unsigned int count = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
 	temp = *head;
 	while (temp)
 	{
 		if (count == idx)
 		{
-			new_node->next = temp;
-			new_node->prev = temp->prev;
-			temp->prev->next = new_node;
-			temp->prev = new_node;
+			new_node = insert_middle(temp, n);
 			break;
 		}
 		count++;
 		temp = temp->next;
 	}
 	if (!temp)
-	{
-		free(new_node);
-		new_node = NULL;
-	}
+		new_node = (idx == count) ? add_dnodeint_end(head, n) : NULL;
+
 	return (new_node);
 }
 
@@ -62,6 +77,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	else
 	{
 		/* Insert node at given index */
-		return (insert_dnodeint(h, idx, n));
+		return (insert_index_not_zero(h, idx, n));
 	}
 }
